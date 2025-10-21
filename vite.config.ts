dce21f7a95e09894b6b95fd3850b86b72af5abf1
Xbro1305 <131260131+Xbro1305@users.noreply.github.com>
2025-10-21 16:00:37 +0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { fileURLToPath} from "node:url";
+import { fileURLToPath } from "node:url";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
-      "@/components": fileURLToPath(new URL("./src/Components", import.meta.url)),
+      "@/components": fileURLToPath(
+        new URL("./src/Components", import.meta.url)
+      ),
       "@/pages": fileURLToPath(new URL("./src/Pages", import.meta.url)),
       "@/api": fileURLToPath(new URL("./src/api", import.meta.url)),
       "@/utils": fileURLToPath(new URL("./src/utils", import.meta.url)),
@@ -32,17 +33,16 @@ export default defineConfig({
         manualChunks: {
           vendor: ["react", "react-dom"],
           router: ["react-router-dom"],
-          query: ["@tanstack/react-query"],
         },
       },
     },
     minify: "terser",
-    terserOptions: ({
+    terserOptions: {
       compress: {
-        drop_console: true, 
+        drop_console: true,
         drop_debugger: true,
       },
-    } as any),
+    } as any,
   },
   optimizeDeps: {
     include: ["react", "react-dom", "react-router-dom"],
